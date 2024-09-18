238767ba147487118ba9d336753438c2e69da46f
Lucas <lucassb395@gmail.com>
2024-09-17 22:54:04 -0300

    Estudos do Curso C# e POO Direto ao Ponto

--- Professor.cs
+++ Professor.cs
@@ -3,7 +3,7 @@
     public class Professor : Pessoa //Classe professor herdando atributos da Classe pessoa
     {
         public Professor(decimal salario, string nome, string documento, DateTime dataNascimento) //Passando por parametro os atributos recebidos da classe pessoa e adicionando os novos
-            : base(nome, documento, dataNascimento) //
+            : base(nome, documento, dataNascimento) // Adicionar explicação futuramente
         {
             Salario = salario; //Inicializando os novos atributos dentro do construtor
             Turmas = new List<string> { "A", "B", "C" };
--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 
 static void Main(string[] args)
 {
-    var professor = new Professor(6500, "Lucas", "XXX.XXX.XXX-XX", new DateTime(1992, 1, 1));
+    var professor = new Professor(6500, "Lucas", "XXX.XXX.XXX-XX", new DateTime(1992, 1, 1)); //Inicialização usando as classes criadas
     var estudante = new Estudante("A", "Luis", "XXX.XXX.XXX-XX", new DateTime(2002, 22, 02));
 
     var pessoas = new List<Pessoa> { estudante, professor };
 
-    foreach (var pessoa in pessoas)
+    foreach (var pessoa in pessoas) //Foreach percorrendo a lista de pessoas criadas acima e usando o método Apresentar em cada uma delas
     {
         pessoa.SeApresentar();
     }
